b06d7e6814fd137a10b29181690af8618492abe1
Xulio-Xulio <xuuulio@gmail.com>
2021-09-27 16:47:19 -0300

    init

--- pokedex/serve.js
+++ pokedex/serve.js
@@ -4,9 +4,9 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 app.listen(port)
 
 console.log('Listening on port: ' + port)
